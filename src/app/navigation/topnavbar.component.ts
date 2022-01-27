232637e78e9d2f8c004e253f0708ff3888ed58a1
aa0233me <aa0233@mix.wvu.edu>
2022-01-26 20:49:22 -0500

    added logo and nav bar

---
+++ src/app/navigation/topnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pcmag-topnavbar',
+    templateUrl: 'topnavbar.Component.html',
+    styleUrls: ['topnavbar.component.css']
+})
+export class TopNavBarComponent {
+
+}
